8b24dea2f1a82891e6e02f481f88f8fd188922ed
qwwqq123 <zzh061221@outlook.com>
2026-03-18 20:42:53 +0800

    Add files via upload
    比较麻烦。

---
+++ 可逆素数/可逆素数/可逆素数.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+int nizhuan(int a)
+{
+	int n;
+	n = a % 10 * 100 + a / 10 % 10 * 10 + a / 100;
+	return n;
+}
+
+int prime(int a)
+{
+	int n = 1;
+	if (a<2) {
+		n = 0;
+	}
+	for (int i = 2; i < a; i++) {
+		if (a % i == 0) {
+			n = 0;
+			break;
+		}
+	}
+	return n;
+}
+
+int main()
+{
+	int m, n;
+	scanf_s("%d %d", &m, &n);
+	for (int i = m; i <= n; i++) {
+		if (prime(i) == 1 &&prime(nizhuan(i)) == 1 && i != nizhuan(i))
+		{
+				if (i < nizhuan(i)) {
+					printf("%d\n", i);
+				}
+				
+			}
+		}
+	return 0;
+	}
+	
